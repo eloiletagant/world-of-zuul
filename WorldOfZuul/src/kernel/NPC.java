6c89132127f0326c5ab0d5064eeb50d16e96d2be
cyprien <cyprien@Cyp>
2016-11-10 18:14:17 +0100

    #NPC

--- WorldOfZuul/src/kernel/NPC.java
+++ WorldOfZuul/src/kernel/NPC.java
@@ -10,6 +10,9 @@ public class NPC extends Character{
 	
 	private boolean enemy; //indicate if the npc is friendly or not 
 	
+	public NPC(String name){
+		super(name);
+	}
 	/**
 	 * This method returns an boolean true if the NPC can fight a character.
 	 * @return
