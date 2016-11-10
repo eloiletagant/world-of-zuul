8fe0b982e8ce3d862b26dda30e7672b003d8a177
theking <theking@172.17.65.13>
2016-11-10 16:22:43 +0100

    commentaires sur les items

--- WorldOfZuul/src/Consumable.java
+++ WorldOfZuul/src/Consumable.java
@@ -1,19 +1,38 @@
-
+/**
+ * 
+ * @author gp4
+ * This class manage consumables. Consumables may have differents effects and are destroyed when they are used.
+ *
+ */
 public class Consumable extends Item{
 
 	private String effect;
-	
+	/**
+	 * Constructor of this class
+	 * @param newName String : name of the item
+	 * @param desc String : description of the item and it's effects
+	 * @param pric Int : price of the item
+	 * @param sell Boolean : is the item sellAble
+	 * @param effects String : effect of the items
+	 */
 	public Consumable(String newName, String desc, int pric, boolean sell,String effects)
 	{
 		super(newName,desc,pric,sell);
 		effect = effects;
 	}
 	
+	/**
+	 * this method use the item : the effects will applies and the item will be destroyed
+	 */
 	public void use()
 	{
 	
 	}
 	
+	/**
+	 * 
+	 * @return String : effect of this consumable
+	 */
 	public String getEffect()
 	{
 		return effect;
--- WorldOfZuul/src/TreasureBox.java
+++ WorldOfZuul/src/TreasureBox.java
@@ -1,9 +1,22 @@
-
+/**
+ * 
+ * @author gp4
+ * This class manage treasure boxes. Boxes contains mmany items and are protected by a Lock
+ *
+ */
 public class TreasureBox extends Item{
 	
 	private Inventory theItems;
 	private Lock blockingLock;
 	
+	/**
+	 * Constructor of the class
+	 * @param newName String : name of the item
+	 * @param desc String : description of the item and it's effects
+	 * @param pric Int : price of the item
+	 * @param sell Boolean : is the item sellAble
+	 * @param aLock Lock : Locket of the chest
+	 */
 	public TreasureBox(String newName, String desc, int pric, boolean sell, Lock aLock)
 	{
 		super(newName, desc, pric, sell);
@@ -11,16 +24,28 @@ public class TreasureBox extends Item{
 		blockingLock = aLock;
 	}
 	
-	public void open()
+	/**
+	 * This method open the chest if the key you try to use is the good one
+	 * @param aKey Key : the key you want to try
+	 */
+	public void open(Key aKey)
 	{
 		
 	}
 	
+	/**
+	 * This method return the chest inventory if the chest is open
+	 * @return the chest inventory
+	 */
 	public Inventory getInventory()
 	{
 		return theItems;
 	}
 	
+	/**
+	 * Access to the chest locket
+	 * @return Lock : chest locket
+	 */
 	public Lock getLock()
 	{
 		return blockingLock;
