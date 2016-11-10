a221fc27308a746f1c6510d7aded9685ae3f3e54
theking <theking@172.17.65.13>
2016-11-10 15:20:49 +0100

    items

--- WorldOfZuul/src/Item.java
+++ WorldOfZuul/src/Item.java
@@ -1,4 +1,34 @@
 
 public class Item {
-
+	private String name;
+	private String description;
+	private int price;
+	private boolean sellAble;
+	//private int weight;
+	
+	public Item(String newName, String desc, int pric, boolean sell)
+	{
+		name = newName;
+		description = desc;
+		price = pric;
+		sellAble = sell;
+	}
+	
+	public String getDescription()
+	{
+		return description;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public int getPrince()
+	{
+		return price;
+	}
+	public boolean getSellAble()
+	{
+		return sellAble;
+	}
+	
 }
