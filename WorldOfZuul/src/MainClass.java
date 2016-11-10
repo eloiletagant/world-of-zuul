d837c54684111d8e12847ecd13db5f4932f6f328
hugob <hugob@172.17.69.226>
2016-11-10 08:54:40 +0100

    hello world

--- WorldOfZuul/src/MainClass.java
+++ WorldOfZuul/src/MainClass.java
@@ -3,7 +3,7 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("test");
+		System.out.println("hello world");
 	}
 }
 
