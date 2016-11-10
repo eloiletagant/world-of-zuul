470c7faa9a16822d4580717c05c78f730fbb8d36
Chloé <Chlo@172.17.69.211>
2016-11-10 17:05:09 +0100

    commentaire

--- WorldOfZuul/src/Lock.java
+++ WorldOfZuul/src/Lock.java
@@ -1,9 +1,29 @@
-
-public class Lock {
-	
+/**
+ * This class manages the lock system of a door or a treasure box.
+ * @author Group4
+ * @version 10/11/2016
+ *
+ */
+public class Lock
+{
 	private Key associatedKey;
 	private boolean lock;
 
-	public boolean getLock() {}
-	public void openLock(Key) {}
+	/**
+	 * This method returns a boolean true if the door or a treasure box is locked
+	 * @return
+	 */
+	public boolean getLock()
+	{
+		return lock;
+	}
+	
+	/**
+	 * This method opens a locked door/treasure box.
+	 */
+	public void openLock(Key)
+	{
+		
+	}
+	}
 }
