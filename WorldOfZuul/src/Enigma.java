1da0f715d2812eb6acf3226dadc074b8c9c6243a
Chloé <Chlo@172.17.69.211>
2016-11-10 17:16:06 +0100

    commentaire

--- WorldOfZuul/src/Enigma.java
+++ WorldOfZuul/src/Enigma.java
@@ -1,4 +1,31 @@
+/**
+ * This method generates an enigma. This enigma will be resolved by the character to win an item or a price.
+ * @author Group 4
+ * @version 10/11/2016
+ *
+ */
+public class Enigma
+{
+	private String question;
+	private String answer;
 
-public class Enigma {
-
+	/**
+	 * Constructor of the class enigma. This method will generate an enigma.
+	 * @param q		a question
+	 * @param a		an answer
+	 */
+	public Enigma(String q, String a)
+	{
+		question = q;
+		answer = a;
+	}
+	
+	/**
+	 * This method will test if the answer is correct.
+	 * @param a
+	 */
+	public void testAnswer (String a)
+	{
+		
+	}
 }
