4cc90af96b56a02d4c0dd74d7d0f86419b7e0e19
Chloé <Chlo@172.17.69.211>
2016-11-10 16:46:11 +0100

    commentaire

--- WorldOfZuul/src/ExitRoom.java
+++ WorldOfZuul/src/ExitRoom.java
@@ -1,7 +1,18 @@
-
+/**
+ * This class manages the door of the room in order to changes the location of a character.
+ * @author Group 4
+ * @version 10/11/2016
+ *
+ */
 public class ExitRoom {
 	private Room nextRoom;
 
-	public Room getNextRoom() {}
-
+	/**
+	 * This method returns the next room of the current room.
+	 * @return
+	 */
+	public Room getNextRoom()
+	{
+		return nextRoom;
+	}
 }
