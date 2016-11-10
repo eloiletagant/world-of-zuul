6a77c7da44b21a8763da5e8ceadd1dc45c130bb1
Chloé <Chlo@172.17.69.211>
2016-11-10 15:54:10 +0100

    commentaire

--- WorldOfZuul/src/Character.java
+++ WorldOfZuul/src/Character.java
@@ -1,4 +1,55 @@
+/**
+ * This class describes and manages a character.
+ * 
+ * @author Groupe4
+ * @version 10/11/2016
+ *
+ */
+public class Character
+{
+	private String name
+	private int health
+	private Inventory inventory
+	private Room location
+	private int nbHandsAvailable
 
-public class Character {
-
+	/**
+	 * This method displays the number of hands which is avaiblable of a character.
+	 */
+	public int getNbHandsAvailable()
+	{
+		
+	}
+	
+	/**
+	 * This method displays the name of the charcater.
+	 */
+	public String getName()
+	{
+		
+	}
+	
+	/**
+	 * This method displays the number of health point of a charcater.
+	 */
+	public int getHealth()
+	{
+		
+	}
+	
+	/**
+	 * This method displays the invertory of the character.
+	 */
+	public Inventory getInventory()
+	{
+		
+	}
+	
+	/**
+	 * This method displays the room where the character is.
+	 */
+	public Room getLocation()
+	{
+		
+	}
 }
