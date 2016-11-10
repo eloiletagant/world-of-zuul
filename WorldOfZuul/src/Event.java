2cd8c9709333c7de70e1e58cd4482bd72b51db84
Chloé <Chlo@172.17.69.211>
2016-11-10 17:21:16 +0100

    commentaire

--- WorldOfZuul/src/Event.java
+++ WorldOfZuul/src/Event.java
@@ -1,4 +1,26 @@
+/**
+ * This class will create an event (enigma, fight, or trade)
+ * @author Group 4
+ * @version 10/11/2016
+ *
+ */
+public class Event
+{
+	private String description;
+	
+	/**
+	 * Constructor of the class event.
+	 */
+	public Event()
+	{
+			
+	}
 
-public class Event {
-
+	/**
+	 * This method will give an item to the character after a win event.
+	 */
+	public void obtainAnItem()
+	{
+		
+	}
 }
