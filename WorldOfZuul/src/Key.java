b30d995714aba56edcd666abd2d41ca4a20e294a
theking <theking@172.17.65.13>
2016-11-10 15:46:35 +0100

    Les clefs

--- WorldOfZuul/src/Key.java
+++ WorldOfZuul/src/Key.java
@@ -1,4 +1,16 @@
 
-public class Key {
-
+public class Key extends Item{
+	
+	Lock associatedLock;
+	
+	public Key(String newName, String desc, int pric, boolean sell, Lock aLock)
+	{
+		super(newName,desc,pric,sell);
+		associatedLock = aLock;
+	}
+	
+	public Lock getLock()
+	{
+		return associatedLock;
+	}
 }
