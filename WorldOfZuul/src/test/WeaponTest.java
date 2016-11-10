dc5ab1ec3e2ce5cb5cd88a274f274e214ea6537c
Anatole <Anatole@172.17.66.38>
2016-11-10 18:30:11 +0100

    up weapon test

--- WorldOfZuul/src/test/WeaponTest.java
+++ WorldOfZuul/src/test/WeaponTest.java
@@ -10,7 +10,7 @@ public class WeaponTest {
     * @Test This method allow testing if a weapon is not equipped.   
     */
     public void testUnequipWeapon(Weapon weap)  throws Exception {
-        assertFalse(unequipeWeapon(weap));
+        assertFalse(unequipWeapon(weap));
     }
 
 }
