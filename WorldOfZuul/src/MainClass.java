c19f1e8f2c3a0f3c632e6d5b0e7c3663c8e768b8
Utilisateur <Utilisateur@172.17.70.197>
2016-11-10 10:00:34 +0100

    vv

--- WorldOfZuul/src/MainClass.java
+++ WorldOfZuul/src/MainClass.java
@@ -3,13 +3,10 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
-<<<<<<< HEAD
-bfqo
-=======
+//coucoubj
+
 		System.out.println("test");
-=======
+
 		System.out.println("hello world coucou !");
->>>>>>> branch 'master' of https://github.com/eloiletagant/WorldOfZuul
 	}
 }
