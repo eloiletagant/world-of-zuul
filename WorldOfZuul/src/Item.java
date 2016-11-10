8d6c24f5d3caf432046c5fb40156dfae82ce658c
Chloé <Chlo@172.17.69.211>
2016-11-10 16:02:38 +0100

    commentaire

--- WorldOfZuul/src/Item.java
+++ WorldOfZuul/src/Item.java
@@ -1,11 +1,20 @@
-
+/**
+ * This class manages the item which is in the inventory of a charcater.
+ * 
+ * @author Groupe 4
+ * @version 10/11/2016
+ *
+ */
 public class Item {
 	private String name;
 	private String description;
 	private int price;
 	private boolean sellAble;
-	//private int weight;
+	private int weight;
 	
+	/**
+	 * constructor of the class item
+	 */
 	public Item(String newName, String desc, int pric, boolean sell)
 	{
 		name = newName;
@@ -14,21 +23,33 @@ public class Item {
 		sellAble = sell;
 	}
 	
+	/**
+	 * This method displays the description of an item.
+	 */
 	public String getDescription()
 	{
 		return description;
 	}
 	
+	/**
+	 * This method displays the name of the item.
+	 */
 	public String getName()
 	{
 		return name;
 	}
 	
+	/**
+	 * This method displays the price of an item.
+	 */
 	public int getPrince()
 	{
 		return price;
 	}
 	
+	/**
+	 * This method informed us if an item can be selled.
+	 */
 	public boolean getSellAble()
 	{
 		return sellAble;
