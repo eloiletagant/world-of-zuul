fa92a82555ea0580358978b1ea42749f49a9a5e7
Chloé <Chlo@172.17.69.211>
2016-11-10 17:40:16 +0100

    commentaire

--- WorldOfZuul/src/Trade.java
+++ WorldOfZuul/src/Trade.java
@@ -1,4 +1,29 @@
-
-public class Trade {
+/**
+ * This method manages a trade between a character and a NPC.
+ * @author Group4
+ * @version 10/11/2016
+ * 
+ */
+public class Trade
+{
+	private NPC aSeller;
+	
+	/**
+	 * Constructor of a trade
+	 */
+	public Trade()
+	{
+		
+	}
+	
+	private void buy()
+	{
+		
+	}
+	
+	private void sell ()
+	{
+		
+	}
 
 }
