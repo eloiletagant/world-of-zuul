4577ad5b3cd7693b7da633a41dd331227124e2aa
theking <theking@172.17.65.13>
2016-11-10 15:50:01 +0100

    les consomables

--- WorldOfZuul/src/Consumable.java
+++ WorldOfZuul/src/Consumable.java
@@ -1,4 +1,21 @@
 
-public class Consumable {
+public class Consumable extends Item{
 
+	String effect;
+	
+	public Consumable(String newName, String desc, int pric, boolean sell,String effects)
+	{
+		super(newName,desc,pric,sell);
+		effect = effects;
+	}
+	
+	public void use()
+	{
+	
+	}
+	
+	public String getEffect()
+	{
+		return effect;
+	}
 }
