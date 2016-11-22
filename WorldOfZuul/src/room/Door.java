c498ced6cbf4e58183856b1e51c7f6384a45d252
Anatole <anatole.gilet@ntymail.com>
2016-11-22 08:54:26 +0100

    update door class

--- WorldOfZuul/src/room/Door.java
+++ WorldOfZuul/src/room/Door.java
@@ -1,21 +1,24 @@
 package room;
 
 /**
- * This class manages the door of the room in order to changes the location of a character.
+ * This class manages the door of the room in order to allow a character to move.
+ * It can be blocked by a lock and so need a key to open it : subclass LockedDoor
  * @author Group 4
  * @version 10/11/2016
  *
  */
-public class ExitRoom {
+public class Door {
+	
+	//the next room of the current one.
 	protected Room nextRoom;
 
-	public ExitRoom(){
+	public Door(){
 		
 	}
 	
 	/**
 	 * This method returns the next room of the current room.
-	 * @return
+	 * @return Room
 	 */
 	public Room getNextRoom()
 	{
