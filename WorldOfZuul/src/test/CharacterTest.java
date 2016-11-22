6f3ac8b77701a74182b07df2768a1a004859c3d7
theking <theking@172.17.65.13>
2016-11-22 10:13:30 +0100

    fezf

--- WorldOfZuul/src/test/CharacterTest.java
+++ WorldOfZuul/src/test/CharacterTest.java
@@ -3,16 +3,23 @@ package test;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import character.Character;
 
 public class CharacterTest {
 
+	
+	@Test
+	public void testCharacter()
+	{
+		Character C1 = new Character("");
+		if(C1.getName() != "Zuul")
+		{
+			fail("Name definition fail");
+		}
+	}
+	
 	@Test
 	public void testAddHealth() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testRemoveHealth() {
 		fail("Not yet implemented");
 	}
 
