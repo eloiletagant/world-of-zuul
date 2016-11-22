664c1a60a161ead10273a8ca24c1a893a181cc5b
cyprien <cyprien@172.17.77.207>
2016-11-22 15:12:22 +0100

    Room

--- WorldOfZuul/src/test/RoomTest.java
+++ WorldOfZuul/src/test/RoomTest.java
@@ -2,13 +2,44 @@ package test;
 
 import static org.junit.Assert.*;
 
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-public class RoomTest {
+import room.Room;
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+
+/**
+ * The test class LockTest
+ * Unit tests of the Lock class 
+ * @author Groupe4
+ * @version 22/11/2016
+ */
+
+public class RoomTest {
+	
+	private Room myRoom;
+	
+	public RoomTest(){
 	}
 
+	@Before
+	public void setUp() {
+		myRoom = new Room("Salle 1", 0);
+	}
+	
+	@After
+	public void tearDown(){}
+	
+	@Test
+	public void testName(){
+		assertEquals("Salle 1",myRoom.getRoomName());
+	}
+	
+	@Test
+	public void testLevel(){
+		assertEquals(0,myRoom.getLevel());
+	}
+	
 }
