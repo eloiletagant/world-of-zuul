cd55db6fd23b1f52dc9cb6087bdeceeeecd41b2e
hugob <hugob@192.168.24.140>
2016-11-22 15:34:49 +0100

    NPC test and new function

--- WorldOfZuul/src/test/NPCTest.java
+++ WorldOfZuul/src/test/NPCTest.java
@@ -4,12 +4,15 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 import character.Character;
-import npc.NPC;
+import character.NPC;
 
 public class NPCTest {
 	
 	private NPC myNPC;
-	
+	/**
+	 * sets up
+	 * <p> rghjk </p>
+	 */
 	@Before
 	public void setUp()
 	{
@@ -24,6 +27,7 @@ public class NPCTest {
 	public void testNameChevalier()
 	{
 		assertEquals("Chevalier au lion",myNPC.getName());
+		assertEquals(true,myNPC.getEnemy());
 	}
 	
 	/**
@@ -34,6 +38,20 @@ public class NPCTest {
 	public void testNameNPC() {
 		character.NPC friend = new NPC ("");
 		assertEquals("Zuul",friend.getName());
+		assertEquals(true,myNPC.getEnemy());
+	}
+	
+	/**
+	 * Method testSwapFriendShip
+	 * <p> checks if the swapping works well </p>
+	 */
+	@Test
+	public void testSwapFriendShip()
+	{
+		myNPC.swapFriendShip();
+		assertEquals(false,myNPC.getEnemy());
+		myNPC.swapFriendShip();
+		assertEquals(true,myNPC.getEnemy());
 	}
 
 }
