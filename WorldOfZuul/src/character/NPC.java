7cc605ab33860a999472e8e60743641d3176db0b
hugob <hugob@172.17.69.226>
2016-11-22 09:12:30 +0100

    Comment NPC

--- WorldOfZuul/src/character/NPC.java
+++ WorldOfZuul/src/character/NPC.java
@@ -2,22 +2,29 @@ package character;
 
 /**
  * This class describes and manages a NPC.
+ * A NPC can be an enemy or a friend.
+ * He have a name and other parameter of a class Character. 
  * 
  * @author Groupe4
  * @version 10/11/2016
  *
  */
+
 public class NPC extends Character{
 	
 	private boolean enemy; //indicate if the npc is friendly or not 
 	
+	/**
+	 * Function NPC he change the name in the class Character.
+	 * @param String name
+	 */
 	public NPC(String name){
 		super(name);
 	}
 	
 	/**
 	 * This method returns an boolean true if the NPC can fight a character.
-	 * @return
+	 * @return boolean enemy
 	 */
 	public boolean getEnemy()
 	{
