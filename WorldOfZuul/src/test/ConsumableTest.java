fda7ccc9997e215db3eb03d7882b8c54d6fbdc2f
Chloé <Chlo@PC-Chlo>
2016-11-22 14:27:21 +0100

    test consumable

--- WorldOfZuul/src/test/ConsumableTest.java
+++ WorldOfZuul/src/test/ConsumableTest.java
@@ -1,14 +1,61 @@
 package test;
-
 import static org.junit.Assert.*;
-
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import item.Consumable;
 
-public class ConsumableTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+/**
+ * The test class ConsumableTest
+ * Unit tests of the Consumable class
+ * @author Group4
+ * @version 22/11/2016
+ *
+ */
+public class ConsumableTest
+{
+	private Consumable myConsumable;
+
+	/**
+	 * Sets up the test fixture.
+	 * Called before every test case method.
+	 * Initialize one consumable for each test, whose name is "Bread".
+	 */
+	@Before
+	public void setUp()
+	{
+		myConsumable=new Consumable("Bread", "This bread was cooked by the baker of the village", 20, true, "This bread get 2 health points to the person who eats it");
+	}
+	
+	/**
+	 * Tears down the test fixture.
+	 * Called after every test case method. Nothing to do in this case.
+	 */
+	@After
+	public void tearDown()
+	{
 	}
 
+	/**
+	 * Method testNameBread
+	 * Checks if the name given as parameter in the constructor is correctly set to the attribute.
+	 * 
+	 */
+	@Test
+	public void testNameBread()
+	{
+		assertEquals("Bread",myConsumable.getName());
+	}
+	
+	/**
+	 * Method testDefaultName
+	 * Checks that the default name is "Potion", if the given name is empty.
+	 */
+	@Test
+	public void testDefaultName()
+	{
+		Consumable bad = new Consumable("","This bread was cooked by the baker of the village", 20, true, "This bread get 2 health points to the person who eats it");
+		assertEquals("Potion",bad.getName());
+	}
 }
