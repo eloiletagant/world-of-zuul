a442dce66ab3e04cd846502aa870d721ae86f634
hugob <hugob@192.168.24.140>
2016-11-22 14:38:07 +0100

    NPC test

--- WorldOfZuul/src/test/NPCTest.java
+++ WorldOfZuul/src/test/NPCTest.java
@@ -3,12 +3,17 @@ package test;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import character.Character;
+import npc.NPC;
 
 public class NPCTest {
+	
+	private NPC npc;
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testNPC() {
+		character.NPC friend = new NPC ("");
+		assertEquals("Zuul",friend.getName());
 	}
 
 }
