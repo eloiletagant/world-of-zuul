28a0f5622e95e56bb0a81c1fee46ce3a60489f20
Cyp2C19 <cyprien8@hotmail.fr>
2016-11-22 09:20:46 +0100

    Update room

--- WorldOfZuul/src/room/Room.java
+++ WorldOfZuul/src/room/Room.java
@@ -6,30 +6,41 @@ import event.Event;
 
 /**
  * 
- * This class describes the structure of a room. 
- * 
+ * This class describes the structure of a room. A room is defined by a name and a map level (0 or 1).
+ * One room could have several doors associated to directions and it can contain some events like fight or trade. 
  * @author Groupe4
  * @version 10/11/2016
  *
  */
-public class Room
-{
-
+public class Room {
+	
 	private String nameRoom;
-	private HashMap<String, ExitRoom> exit;
+	private HashMap<String, Door> exitDoors;
 	private ArrayList<Event> events;
 	private int level;
 
 	/**
-	 * This method displays exits from the room where the player is.
-	 */
-	public HashMap<String, ExitRoom> getExitRoom()
-	{
-		return exit;
+	 * Constructor used to instantiate a room
+	 * @param String name: Room name
+	 * @param int level: Room level
+	*/
+	public Room(String name, int level){
+		this.name = name;
+		this.level = level;
 	}
 	
 	/**
-	 * This methods displays the different events.
+	 * Simple accessor
+	 * @return HashMap<String, Door>: doors associated to the room
+	 */
+	public HashMap<String, Door> getExitDoors()
+	{
+		return exitDoors;
+	}
+	
+	/**
+	 * Simple accessor
+	 * @return ArrayList<Event>: Events associated to the room
 	 */
 	public ArrayList<Event> getEvents()
 	{
@@ -37,7 +48,8 @@ public class Room
 	}
 	
 	/**
-	 * This method displays the name of the room.
+	 * Simple accessor
+	 * @return String: Room name
 	 */
 	public String getRoomName()
 	{
@@ -45,7 +57,8 @@ public class Room
 	}
 	
 	/**
-	 * This method displays the level of the room.
+	 * Simple accessor
+	 * @return int: Room level
 	 */
 	public int getLevel()
 	{
