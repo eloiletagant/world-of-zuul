acd4c3bb992cc2aaa85ffa9b1ae8a4afcd3cf110
hugob <hugob@192.168.24.140>
2016-11-22 15:00:31 +0100

    NPC test comment

--- WorldOfZuul/src/test/NPCTest.java
+++ WorldOfZuul/src/test/NPCTest.java
@@ -1,17 +1,37 @@
 package test;
 
 import static org.junit.Assert.*;
-
+import org.junit.Before;
 import org.junit.Test;
 import character.Character;
 import npc.NPC;
 
 public class NPCTest {
 	
-	private NPC npc;
-
+	private NPC myNPC;
+	
+	@Before
+	public void setUp()
+	{
+		myNPC = new NPC ("Chevalier au lion");
+	}
+	
+	/**
+	 * Method testNameChevalier
+	 * <p> checks if the name given as parameter in the constructor is correctly set to the attribute </p>
+	 */
 	@Test
-	public void testNPC() {
+	public void testNameChevalier()
+	{
+		assertEquals("Chevalier au lion",myNPC.getName());
+	}
+	
+	/**
+	 * Method testNameNPC
+	 * <p> checks that the default name is Zuul, if the given name is empty</p>
+	 */
+	@Test
+	public void testNameNPC() {
 		character.NPC friend = new NPC ("");
 		assertEquals("Zuul",friend.getName());
 	}
