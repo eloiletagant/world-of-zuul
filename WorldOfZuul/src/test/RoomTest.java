326a09e66436950555dbf0fe828a91f43c305d3a
theking <theking@172.17.65.13>
2016-11-22 17:04:03 +0100

    Room un peu avancée, manque 3 fonctions de teste a faire

--- WorldOfZuul/src/test/RoomTest.java
+++ WorldOfZuul/src/test/RoomTest.java
@@ -2,12 +2,14 @@ package test;
  
 import static org.junit.Assert.*;
 
+import java.util.HashMap;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import room.Room;
+import room.Door;
 
 
 /**
@@ -53,6 +55,32 @@ public class RoomTest {
 	public void testNegativeLevel(){
 		Room myRoom2 = new Room("Salle 2", -2);
 		assertEquals(0,myRoom2.getLevel());
+	}
+	
+	@Test
+	public void testAddExit()
+	{
+		Room room2 = new Room("Salle 2", 1);
+		Room room3 = new Room("Salle 3", 1);
+		Door d1 = new Door(room2);
+		Door d2 = new Door(room2);
+		Door d3 = new Door(room3);
+		Door d4 = new Door(myRoom);
+		myRoom.addExit(d1, "front");
+		HashMap<String, Door> map = new HashMap<String, Door>();
+		map.put("front", d1);
+		assertEquals(myRoom.getExitDoors(),map);
+		myRoom.addExit(d1, "front");
+		assertEquals(myRoom.getExitDoors(),map);
+		myRoom.addExit(d2, "right");
+		assertEquals(myRoom.getExitDoors(),map);
+		myRoom.addExit(d3, "front");
+		assertEquals(myRoom.getExitDoors(),map);
+		myRoom.addExit(d3, "right");
+		map.put("right", d3);
+		assertEquals(myRoom.getExitDoors(),map);
+		myRoom.addExit(d4, "left");
+		assertEquals(myRoom.getExitDoors(),map);
 	}
 		
 	
