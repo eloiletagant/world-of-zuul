323c6f9b680c9f23da28da6734821624b8d54f1c
Chloé <Chlo@172.17.69.211>
2016-11-23 12:02:55 +0100

    marche pas

--- WorldOfZuul/src/test/InventoryTest.java
+++ WorldOfZuul/src/test/InventoryTest.java
@@ -1,4 +1,5 @@
 package test;
+import item.Item;
 import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
@@ -16,6 +17,7 @@ import item.Inventory;
 public class InventoryTest
 {
 	private Inventory myInventory;
+	private Item itemToAdd;
 
 	/**
 	 * Sets up the test fixture.
@@ -35,5 +37,38 @@ public class InventoryTest
 	@After
 	public void tearDown()
 	{
-	}	
+	}
+	
+	/**
+	 * Method testUse
+	 * <p>Checks if an item is correctly add in an inventory</p>
+	 */
+	@Test
+	public void testAddItem()
+	{
+		//ça marche pas j'arrive pas à créer d'item vu que c'est en abstract
+		itemToAdd = new Item("Unidentified object", "This object seems strange and come from nowhere.", 100, true);
+		myInventory.addItem(itemToAdd);
+		
+	}
+	
+	/**
+	 * Method testUseDestroyItem
+	 * <p>Checks if an item is correctly delete from an inventory</p>
+	 */
+	@Test
+	public void testDeleteItem()
+	{
+		
+	}
+	
+	/**
+	 * Method testResetItem
+	 * <p>Checks if the inventory is correctly clear</p>
+	 */
+	@Test
+	public void testResetItem()
+	{
+		
+	}
 }
