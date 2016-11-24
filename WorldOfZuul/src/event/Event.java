c5ab3c0853f1d0e36f4d5a9cf295b943c93cecbf
Anatole <anatole.gilet@ntymail.com>
2016-11-24 11:40:01 +0100

    improve event and enigma

--- WorldOfZuul/src/event/Event.java
+++ WorldOfZuul/src/event/Event.java
@@ -1,40 +1,40 @@
 package event;
 
 import item.Item;
+import character.Character;
 
 /**
  * This class will create an event (enigma, fight, or trade)
+ *
  * @author Group 4
  * @version 10/11/2016
- *
  */
-public class Event
-{
-	protected String description;
-	protected Item loot;
-	protected Character player;
-	
-	/**
-	 * Constructor of the class event.
-	 */
-	public Event(String description, Item loot, Character player) {
-		this.description = description;
-		this.loot = loot;
-		this.player = player;
-	}
+public class Event {
+    protected String description;
+    protected Item loot;
+    protected Character player;
 
-	/**
-	 * This method will give an item to the character after a win event.
-	 */
-	public Item getLoot() {
-		return loot;
-	}
-	
-	/**
-	 * This method will set a new item for the event loot.
-	 */
-	public void setLoot(Item loot) {
-	
-	}
+    /**
+     * Constructor of the class event.
+     */
+    public Event(String description, Item loot, Character player) {
+        this.description = description;
+        this.loot = loot;
+        this.player = player;
+    }
+
+    /**
+     * This method will give an item to the character after a win event.
+     */
+    public Item getLoot() {
+        return loot;
+    }
+
+    /**
+     * This method will set a new item for the event loot.
+     */
+    public void setLoot(Item loot) {
+
+    }
 
 }
