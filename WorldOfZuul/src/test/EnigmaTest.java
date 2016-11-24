45cd60d5b2f6c94c0540c8c5f6f2573905436fe2
hugob <hugob@172.17.69.226>
2016-11-24 11:55:50 +0100

    progress in enigma test

--- WorldOfZuul/src/test/EnigmaTest.java
+++ WorldOfZuul/src/test/EnigmaTest.java
@@ -2,19 +2,53 @@ package test;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
-public class EnigmaTest {
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import event.Enigma;
 
-	@Test
+public class EnigmaTest {
+	
+	private Enigma myEnigma;
+	/**
+	 * sets up
+	 * <p> Initialize one Enigma for each test </p>
+	 */
+	@Before
+	public void setUp()
+	{
+		myEnigma = new Enigma ("Enigme a la con",potion,player,"1+1=?","2");
+	}
+	
     /**
      * Method testTestAnswer
-     * This method checks if the method of comparison of answer works well.
+     * <p>This method checks if the method of comparison of answer works well.</p>
      */
-	public void testTestAnswer() throws Exception {
-		 Enigma a= new Enigma("chaussettes");
-	     assertTrue(testAnswer(a));
-	     Enigma a= new Enigma("other");
-	     assertFalse(a.testAnswer(testAnswer(a));
+	@Test
+	public void testTestAnswer()
+	{
+	     assertTrue(myEnigma.testAnswer("2"));
+	     assertFalse(myEnigma.testAnswer("11"));
+	}
+	
+    /**
+     * Method testSetAnswer
+     * <p>This method checks if the input in setAnswer works well</p>
+     */
+	public void testSetAnswer()
+	{
+		assertEquals("2",myEnigma.setAnswer("2"));
+	}
+	
+    /**
+     * Method testSetQuestion
+     * <p>This method checks if the input in setQuestion works well</p>
+     */
+	public void testSetQuestion()
+	{
+		assertEquals("1+1=?",myEnigma.setQuestion("1+1=?"));
 	}
 }
