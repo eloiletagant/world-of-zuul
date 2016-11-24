e8319de84e058307dfb7ab1fb1946ab7e127d9ed
Marine <Marine@DESKTOP-9Q9FVGA>
2016-11-24 11:35:46 +0100

    ..

--- WorldOfZuul/src/Interface.java
+++ WorldOfZuul/src/Interface.java
@@ -27,50 +27,147 @@ public class Interface extends JFrame {
     //button building
     protected JButton myButton1= new JButton("PLAY");
     protected JButton myButton2= new JButton("SETTING");
+    protected JButton left = new JButton("left");
+    //protected JButton right = new JButton ("right");
+    //protected JButton front= new JButton("front");
+    protected JButton back = new JButton ("back");
+    protected JButton right,front;
+    protected Icon myIcon;
     
-    public Interface ()
+    
+    public Interface (String salle)
     {
-        
-        Icon myIcon= new ImageIcon("./chateau.jpg");
-        
-                //(new javax.swing.ImageIcon(getClass().getResource("chateau.jpg"))).getImage();
-       
+        //Icon myIcon= new ImageIcon("./chateau.jpg");
         JPanel myPanel = new JPanel(new BorderLayout ());
-        JPanel panelBouton = new JPanel (new GridLayout (2,1));
+        JPanel panelButton = new JPanel (new BorderLayout ());
+        JPanel Button = new JPanel (new GridLayout (2,1));
+        if (salle == "Acceuil")
+        {        
+            Icon myIcon= new ImageIcon("./chateau.jpg");
         
-        //Mise d'une image en fond
-        JLabel label = new JLabel (myIcon);
+            //(new javax.swing.ImageIcon(getClass().getResource("chateau.jpg"))).getImage();
+       
         
-        //CRéatio bouton play
-        //Buttons design
-        Font police = new Font ("Kristen ITC", Font.BOLD, 14);
+            //Mise d'une image en fond
+            JLabel label = new JLabel (myIcon);
+        
+            //Créatio bouton play
+            //Buttons design
+            Font police = new Font ("Kristen ITC", Font.BOLD, 14);
        
 
-        myButton1.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.BLACK));
-        myButton1.setBackground(Color.black);
-        myButton1.setForeground(Color.white);
-        myButton1.setFont(police);
+            myButton1.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.BLACK));
+            myButton1.setBackground(Color.black);
+            myButton1.setForeground(Color.white);
+            myButton1.setFont(police);
         
-        myButton2.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.BLACK));
-        myButton2.setBackground(Color.black);
-        myButton2.setForeground(Color.yellow);
-        myButton2.setFont(police);
+            myButton2.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.BLACK));
+            myButton2.setBackground(Color.black);
+            myButton2.setForeground(Color.yellow);
+            myButton2.setFont(police);
         
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setTitle("WOZ");
+            this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            this.setTitle("WOZ");
         
-        panelBouton.add(myButton1,BorderLayout.WEST);
-        panelBouton.add(myButton2,BorderLayout.EAST);
+            panelButton.add(myButton1,BorderLayout.WEST);
+            panelButton.add(myButton2,BorderLayout.EAST);
         
-        myPanel.add(label,BorderLayout.NORTH);
-        myPanel.add(panelBouton, BorderLayout.SOUTH);
+            myPanel.add(label,BorderLayout.NORTH);
+            myPanel.add(panelButton, BorderLayout.SOUTH);
         
-                
-        this.add(myPanel);
-        this.setSize(500, 500);
-        this.pack();
-        this.setVisible(true);       
-    }
- 
- 
+        
+        
+        }   
+        else if (salle == "3Doors")
+        {        
+            Icon myIcon= new ImageIcon("./Room3Doors.jpg");
+            Icon flecheRIght = new ImageIcon("./Fdroite.jpg");
+            Icon flecheFront = new ImageIcon("./Fhaut.jpg");
+            Icon flecheRecul = new ImageIcon ("./Fbas.jpg");
+            Icon flecheGauche = new ImageIcon("./Fgauche.jpg");
+            //(new javax.swing.ImageIcon(getClass().getResource("chateau.jpg"))).getImage();
+       
+        
+            //Mise d'une image en fond
+            JLabel label = new JLabel (myIcon);
+        
+      
+
+            right= new JButton(flecheRIght);
+            right.setBackground(Color.black);
+            front = new JButton(flecheFront);
+            front.setBackground(Color.black);
+            back = new JButton(flecheRecul);
+            back.setBackground(Color.black);
+            left = new JButton(flecheGauche);
+            left.setBackground(Color.black);
+          
+            left.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.BLACK));
+            right.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.BLACK));
+            back.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.BLACK));
+            front.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.BLACK));
+       
+        
+        
+            panelButton.add(left,BorderLayout.WEST);
+            panelButton.add(right,BorderLayout.EAST);
+            panelButton.add(front,BorderLayout.NORTH);
+            panelButton.add(back,BorderLayout.SOUTH);
+        
+            myPanel.add(label,BorderLayout.NORTH);
+            myPanel.add(panelButton, BorderLayout.SOUTH);
+        
+        
+        
+         }  
+         else if (salle == "2Doors")
+        {        
+            Icon myIcon= new ImageIcon("./Room2Doors.png");
+            Icon flecheRIght = new ImageIcon("./Fdroite.jpg");
+            Icon flecheFront = new ImageIcon("./Fhaut.jpg");
+            Icon flecheRecul = new ImageIcon ("./Fbas.jpg");
+            Icon flecheGauche = new ImageIcon("./Fgauche.jpg");
+            //(new javax.swing.ImageIcon(getClass().getResource("chateau.jpg"))).getImage();
+       
+
+            //Mise d'une image en fond
+            JLabel label = new JLabel (myIcon);
+
+            right= new JButton(flecheRIght);
+            right.setBackground(Color.black);
+            front = new JButton(flecheFront);
+            front.setBackground(Color.black);
+            back = new JButton(flecheRecul);
+            back.setBackground(Color.black);
+            left = new JButton(flecheGauche);
+            left.setBackground(Color.black);
+          
+            left.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.BLACK));
+            right.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.BLACK));
+            back.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.BLACK));
+            front.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.BLACK));
+       
+        
+            panelButton.add(left,BorderLayout.WEST);
+            panelButton.add(right,BorderLayout.EAST);
+            panelButton.add(front,BorderLayout.NORTH);
+            panelButton.add(back,BorderLayout.SOUTH);
+        
+            myPanel.add(label,BorderLayout.NORTH);
+            myPanel.add(panelButton, BorderLayout.SOUTH);
+        
+        
+        
+         } 
+         
+      this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      this.setTitle("WOZ");    
+      this.add(myPanel);
+      this.setSize(500, 500);
+      this.pack();
+      this.setVisible(true);
+ }
+    
+  
+
 }
