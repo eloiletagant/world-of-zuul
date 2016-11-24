4f841a0fc6b95f5761eaac124b33b96333dfd403
hugob <hugob@172.17.69.226>
2016-11-24 12:00:41 +0100

    test enigma 2

--- WorldOfZuul/src/test/EnigmaTest.java
+++ WorldOfZuul/src/test/EnigmaTest.java
@@ -4,10 +4,6 @@ import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
-import org.junit.Before;
-import org.junit.Test;
 import event.Enigma;
 
 public class EnigmaTest {
