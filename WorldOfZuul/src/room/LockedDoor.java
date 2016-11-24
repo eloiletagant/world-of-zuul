09044da2d0fa2086b0351c9045b8b099e0043034
cyprien <cyprien@172.17.77.207>
2016-11-24 11:25:23 +0100

    lockeddoor

--- WorldOfZuul/src/room/LockedDoor.java
+++ WorldOfZuul/src/room/LockedDoor.java
@@ -1,6 +1,5 @@
 package room;
 
-import item.Key;
 import item.Lock;
 
 /**
@@ -14,19 +13,13 @@ public class LockedDoor extends Door{
 
 	//this is the lock which allow to keep the door close.
 	private Lock lock;
-	//the key associate to the lock
-	private Key key;
-	//state of the lock
-	private boolean isLocked;
 
 	/**
 	 * Constructor which need the informations about the rooms (current and next one)
 	 * and all informations about the key
 	 */
-	public LockedDoor(Room nextRoom, String keyName, String keyDescription, int keyPrice, boolean keySellAble){
+	public LockedDoor(Room nextRoom, Lock lock){
 		super(nextRoom);
-		lock = new Lock(keyName, keyDescription, keyPrice, keySellAble);
-		isLocked = true; //the door is closed by default
 	}
 	
 	
@@ -36,25 +29,6 @@ public class LockedDoor extends Door{
 	 */
 	public Lock getLock(){
 		return lock;
-	}
-	
-	/**
-	 * This method return the state lock or unlock of a lockedDoor.
-	 * @return the state of the lock
-	 */
-	public boolean isLocked(){
-		return isLocked;
-	}
-	
-	/**
-	 * This method locks or unlocks a lockedDoor, so allow the character to move away;
-	 */
-	public void lockingDoor(){
-		if (isLocked) {
-			isLocked = false;
-		} else {
-			isLocked = true;
-		}
 	}
 
 }
