ae0f0e54e93e0f8bd4f9c21e2309c5704b816d2c
Marine <Marine@DESKTOP-9Q9FVGA>
2016-11-24 08:48:58 +0100

    Test

---
+++ WorldOfZuul/src/Interface.java
@@ -0,0 +1,34 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+
+import java.awt.event.ActionEvent;
+
+import java.awt.event.ActionListener; 
+
+import javax.swing.JFrame;
+
+import javax.swing.JLabel;
+import java.awt.GridLayout;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.util.Scanner;
+
+
+public class Interface extends JFrame {
+	
+	Image myImage;
+	
+	public Interface ()
+	{
+		private JFrame myFrame;
+		//button building
+	    protected JButton myButton1= new JButton("PLAY");
+	    protected JButton myButton1= new JButton("SETTING");
+	    myImage= (new javax.swing.ImageIcon(getClass().getResource()))
+	    
+	}
+	
+	
+
+}
