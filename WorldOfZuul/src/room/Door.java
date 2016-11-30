455ad8343ae858de16498575a234234f57c4a4af
Anatole <anatole.gilet@ntymail.com>
2016-11-30 11:23:34 +0100

    door class ok

--- WorldOfZuul/src/room/Door.java
+++ WorldOfZuul/src/room/Door.java
@@ -4,28 +4,29 @@ package room;
  * This class manages the door of the room in order to allow a character to move.
  * It can be blocked by a lock and so need a key to open it : subclass LockedDoor
  * Have to check the player move in another place.
- * 
+ *
  * @author Group 4
  * @version 10/11/2016
- *
  */
 public class Door {
-	
-	//the next room of the current one, it can exist just one next room
-	protected Room nextRoom;
 
-	/**
-	 * Constructor of the door class, there is just one room given in parameters
-	 */
-	public Door(Room nextRoom){
-		this.nextRoom = nextRoom;
-	}
+    //the next room of the current one, it can exist just one next room
+    protected Room nextRoom;
 
-	/**
-	 * This method returns the next room of the current room.
-	 * @return Room
-	 */
-	public Room getNextRoom(){
-		return nextRoom;
-	}
+    /**
+     * Constructor of the door class, there is just one room given in parameters
+     * @param nextRoom Room room on the other side of the door
+     */
+    public Door(Room nextRoom) {
+        this.nextRoom = nextRoom;
+    }
+
+    /**
+     * This method returns the next room of the current room.
+     *
+     * @return Room
+     */
+    public Room getNextRoom() {
+        return nextRoom;
+    }
 }
