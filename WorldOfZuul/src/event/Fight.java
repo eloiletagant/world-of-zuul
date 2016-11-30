9289e754a8fd2df02b198226349b5443f2199102
Anatole <anatole.gilet@ntymail.com>
2016-11-30 11:35:28 +0100

    constructor of fight class

--- WorldOfZuul/src/event/Fight.java
+++ WorldOfZuul/src/event/Fight.java
@@ -1,22 +1,29 @@
 package event;
 
+import character.Character;
 import character.NPC;
+import item.Item;
 
 /**
  * This class manages a fight between a character and a NPC.
+ *
  * @author Group4
  * @version 10/11/2016
- *
  */
-public class Fight extends Event
-{
-	private NPC anAdversary;
+public class Fight extends Event {
+    private boolean isWin;
 
-	/**
-	 * Constructor of a fight
-	 */
-	public Fight()	
-	{
-		
-	}
+    /**
+     * Constructor of a fight
+     *
+     * @param description String description of the event
+     * @param item Item which can be get by the player, or trade
+     * @param player Character the player of the game
+     * @param npc NPC which is involved in the event
+     */
+    public Fight(String description, Item item, Character player, NPC npc) {
+        super(description, item, player, npc);
+		isWin = false;
+    }
+
 }
