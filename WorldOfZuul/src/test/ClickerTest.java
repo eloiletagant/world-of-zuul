906afc70a1fa98aa035f001dc1db0139bbb1c436
theking <theking@172.17.65.13>
2016-12-01 17:51:03 +0100

    rezgfsdggdsfg

--- WorldOfZuul/src/test/ClickerTest.java
+++ WorldOfZuul/src/test/ClickerTest.java
@@ -1,11 +1,6 @@
 package test;
-import static org.junit.Assert.*;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
 import character.*;
-import room.Room;
 import clickerGame.Clicker;
 import event.*;
 import item.Weapon;
@@ -14,11 +9,7 @@ public class ClickerTest
 {
 private Fight f1;
 	
-	/**
-	 * Initialize the character with the name "pseudo"
-	 */
-	@Before
-	public void setUp()
+	public void main()
 	{
 		Player c1 = new Player("Gael");
 		NPC lapin = new NPC("TruiteVelue",60,2,true);
@@ -29,17 +20,14 @@ private Fight f1;
 		c1.getInventory().addItem(w2);
 		lapin.getInventory().addItem(w1);
 		f1 = new Fight("C'est l'heure du duel !", w2, c1, lapin);
+		testClicks();
 	}
-	
-	@Test
-	/**
-	 * 
-	 */
+
 	public void testClicks()
 	{
 		int nbclicks = Clicker.clickerLauncher(30);
 		boolean result = f1.runFight(nbclicks, 30);
-		assertEquals(true, result);
+		System.out.println(result);
 	}
 	
 }
