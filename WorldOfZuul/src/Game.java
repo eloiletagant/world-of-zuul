f9d80368fe479972885583fe92c9fc618f24702d
cyprien <cyprien@Cyp>
2016-12-01 10:39:44 +0100

    game

--- WorldOfZuul/src/Game.java
+++ WorldOfZuul/src/Game.java
@@ -68,7 +68,7 @@ public class Game {
      */
     private void printWelcome() {
         System.out.println();
-        System.out.println("Welcome to the World of Zuul!");
+        System.out.println("Welcome to the World of Zuul" + player.getName() + " !!");
         System.out.println("World of Zuul is a new, incredibly boring adventure game.");
         System.out.println("Type 'help' if you need help.");
         System.out.println();
