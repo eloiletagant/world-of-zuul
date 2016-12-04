2823df5d772a2db9cbf39fe3c3c6846bf6c530b0
cyprien <cyprien@172.16.15.1>
2016-12-04 09:45:45 +0100

    inventory

--- WorldOfZuul/src/InventoryInterfaceListener.java
+++ WorldOfZuul/src/InventoryInterfaceListener.java
@@ -136,10 +136,11 @@ public class InventoryInterfaceListener extends MouseAdapter
     	    		 {
     	    			 if (door.getValue().isLocked()==true)
     	    			 {
-    	    				 if (door.getValue().getLock().unlock((Key) myItem)==true)
+    	    				 if (door.getValue().getLock().unlock((Key) myItem)==true){
     	    					 System.out.println("good key");
     	    				 	 inventory.getGame().manageDirectionButtons();
     	    					 break;
+    	    				 }
     	    			 }
     	    		 }
 //    	    		 if ((doors.containsKey("front") && doors.get("front").isLocked() == true) || (doors.containsKey("behind") && doors.get("behind").isLocked() == true) || (doors.containsKey("left") && doors.get("left").isLocked() == true) || (doors.containsKey("right") && doors.get("right").isLocked() == true))
