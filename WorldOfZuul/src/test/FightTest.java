ce646a2fb1584f1963eaefd6bdabb2aebfec0d68
Chloé <Chlo@192.168.1.11>
2016-12-05 21:19:00 +0100

    correction test fight

--- WorldOfZuul/src/test/FightTest.java
+++ WorldOfZuul/src/test/FightTest.java
@@ -22,7 +22,7 @@ public class FightTest {
 	{
 		Player c1 = new Player("Gael");
 		NPC lapin = new NPC("TruiteVelue",60,2,true);
-		Weapon w1 = new Weapon("Epee de merde", "Elle roks du poney", 1, true, 2, true);
+		Weapon w1 = new Weapon("Epee de merde", "Elle roks du poney", 1, true, 2);
 		w1.equip();
 		c1.getInventory().addItem(w1);
 		Weapon w2 = w1;
