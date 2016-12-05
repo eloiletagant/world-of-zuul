c01b6effe958e985bb7f427f09c2132587796a0e
Utilisateur <Utilisateur@192.168.1.53>
2016-12-05 16:10:03 +0100

    zmoa

--- WorldOfZuul/src/Game.java
+++ WorldOfZuul/src/Game.java
@@ -654,7 +654,7 @@ public class Game extends JFrame {
     public void manageEgnimButton ()
     {
         if (player.getLocation() == rooms.get(4) || player.getLocation() == rooms.get(9) || player.getLocation() == rooms.get(27))
-        {
+        { 
         	//System.out.print("noimom"); 
             speak.setEnabled(true);
         }               
