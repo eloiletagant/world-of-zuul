bf0e06c88939d0e77b7ce2076c993b1067ea12fb
hugob <hugob@172.17.69.226>
2016-12-07 11:01:48 +0100

    dialogue

--- WorldOfZuul/src/Game.java
+++ WorldOfZuul/src/Game.java
@@ -827,7 +827,7 @@ public class Game extends JFrame {
         {
         	textDescRoom.setText("Welcome in this dungeon! Are you ready to fight this terrific world ? You must kill the boss to save the world and loot the famous treasure! You will have to find your path in this creepy labyrinth! But your path will be strewn with pitfalls. To access the boss, you will have to answer several enigma by staying alive. But it will be not easy because you will meet a lot of beasts. To help you get started you just earned 50 pieces. Use them wisely. Good luck …");
         }
-        else if (currentRoom==rooms.get(24))
+        else if (currentRoom==rooms.get(24)) 
         {
         	textDescRoom.setText("Must stop this bullshit from north and south! Once and for all, the north, depending on our orientation, it changes everything!");
         }
